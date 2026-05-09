5d1d8bb0bd469ee0aa8bebd5b070481070a60652
MohammedAdnan04 <mohammedadnan04@users.noreply.github.com>
2026-05-09 17:04:24 +0100

    thubnail

--- src/data/generated/available_img.js
+++ src/data/generated/available_img.js
@@ -1,1 +1,1 @@
-var available_img = ["alarifi2023enhanced", "gedam2021review", "huda2020lowcost", "islam2023deep", "li2020bilstm", "prietoavalos2022wearable", "radha2021deep", "sabry2022machine", "shaik2023remote", "shajari2023emergence"];
+const availableImg = ["alarifi2023enhanced","Beck2016Visual","gedam2021review","huda2020lowcost","islam2023deep","li2020bilstm","prietoavalos2022wearable","radha2021deep","sabry2022machine","shaik2023remote","shajari2023emergence"];
